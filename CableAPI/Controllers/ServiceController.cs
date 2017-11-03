8c1d1a95526201c08ea8d9bf78483194ca179af3
Pragnesh Patel <pragnesh.patel@viprak.com>
2017-11-03 13:19:44 +0530

    Add Models

---
+++ CableAPI/Controllers/ServiceController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CableAPI.Data;
+
+namespace CableAPI.Controllers
+{
+	public class ServiceController : Controller
+	{
+		TimeWarnerEntities _context = new TimeWarnerEntities();
+		// GET: Service
+		public ActionResult Index(string UserName, string Password)
+		{
+			UserName = "admin@4tpp.com";
+			Password = "test123";
+			var UserData = _context.p_VerifyUser(UserName, Password).ToList();
+			return View();
+		}
+	}
+}
