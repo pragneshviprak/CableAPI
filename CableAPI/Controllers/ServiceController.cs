a1292729cb6226e9d717356d280ec14b8d9d0774
Pragnesh Patel <pragnesh.patel@viprak.com>
2017-11-03 13:42:07 +0530

    Add Return Result

--- CableAPI/Controllers/ServiceController.cs
+++ CableAPI/Controllers/ServiceController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using CableAPI.Data;
+using CableAPI.Models;
 
 namespace CableAPI.Controllers
 {
@@ -11,12 +12,15 @@ namespace CableAPI.Controllers
 	{
 		TimeWarnerEntities _context = new TimeWarnerEntities();
 		// GET: Service
-		public ActionResult Index(string UserName, string Password)
+
+		[HttpGet]
+		public JsonResult Index(string UserName, string Password)
 		{
-			UserName = "admin@4tpp.com";
-			Password = "test123";
+			//UserName = "admin@4tpp.com";
+			//Password = "test123";
 			var UserData = _context.p_VerifyUser(UserName, Password).ToList();
-			return View();
+			return new ReturnFormat().Success(Message.SUCCESS, UserData);
 		}
 	}
 }
+
---
+++ CableAPI/Models/ReturnFormat.cs
@@ -0,0 +1,66 @@
+﻿using CableAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Text;
+
+namespace CableAPI.Models
+{
+	public class ReturnFormat
+	{
+		public class Reply
+		{
+			public string status { get; set; }
+			public string msg { get; set; }
+			public string error { get; set; }
+			public dynamic data { get; set; }
+		}
+
+		public JsonResult Success(string txt, dynamic data = null)
+		{
+			return PrepareReply(false, txt, data);
+		}
+
+		public JsonResult Error(string txt)
+		{
+			return PrepareReply(true, txt);
+		}
+
+		public JsonResult PrepareReply(bool isError, string txt, dynamic data = null)
+		{
+			var reply = new Reply
+			{
+				status = isError ? Message.FAIL : Message.SUCCESS,
+				msg = isError ? "" : txt,
+				error = isError ? txt : null,
+				data = data,
+			};
+			return new JsonNetResult(reply, JsonRequestBehavior.AllowGet);
+		}
+	}
+
+	public class Message
+	{
+		public const string FAIL = "Fail";
+		public const string SUCCESS = "Success";
+	}
+
+	public class JsonNetResult : JsonResult
+	{
+		private JsonNetResult()
+		{
+			this.ContentType = "application/json";
+		}
+
+		public JsonNetResult(object data, JsonRequestBehavior jsonRequestBehavior)
+		{
+			this.ContentEncoding = Encoding.UTF8;
+			this.ContentType = "application/json";
+			this.Data = data;
+			this.JsonRequestBehavior = jsonRequestBehavior;
+		}
+	}
+
+}
